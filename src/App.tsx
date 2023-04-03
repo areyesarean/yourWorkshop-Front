95e31f5669ddfbd47f477cbba79059512007b890
areyesarean <alereyarean@gmail.com>
2023-04-03 04:25:50 +0200

    install react-query and axios and add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const darkTheme = createTheme({
   palette: {
@@ -13,10 +16,12 @@ const darkTheme = createTheme({
 function App() {
   return (
     <Suspense fallback={<h1>Cargando</h1>}>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </Suspense>
   );
 }
