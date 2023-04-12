9b84871054ce1b23a177e759f8984af32c4307c7
areyesarean <alereyarean@gmail.com>
2023-04-12 15:37:02 +0200

    feat: create mainMenu
    Agrupamos toda la logica del menu en un componente

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,94 @@
+import { styled } from "@mui/material/styles";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import MenuIcon from "@mui/icons-material/Menu";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ExitToApp from "@mui/icons-material/ExitToApp";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/slices/AuthSlice";
+
+const drawerWidth: number = 240;
+
+interface AppBarCustomProps {
+  toggleDrawer: () => void;
+  open: boolean;
+}
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+function AppBarCustom({ toggleDrawer, open }: AppBarCustomProps) {
+  const dispatch = useDispatch();
+
+  const handleLoggout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <AppBar position="absolute" open={open} color="inherit">
+      <Toolbar
+        sx={{
+          pr: "24px", // keep right padding when drawer closed
+        }}
+      >
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={toggleDrawer}
+          sx={{
+            marginRight: "36px",
+            ...(open && { display: "none" }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1 }}
+        >
+          Tu taller
+        </Typography>
+        <IconButton color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <IconButton
+          color="inherit"
+          title="Cerrar Sesion"
+          onClick={handleLoggout}
+        >
+          <ExitToApp />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default AppBarCustom;
