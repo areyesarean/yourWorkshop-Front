bf42ddc806b053664eaed67f370755a1e83a5500
areyesarean <alereyarean@gmail.com>
2023-04-13 15:16:37 +0200

    feat: create configSlice
    el configSlice contiene el estado global de la configuracion de la app y
    el modo claro o oscuro

---
+++ src/redux/slices/ConfigSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  getDarkModeFromLocalStorage,
+  removeDarkModeFromLocalStorage,
+  setDarkModeFromLocalStorage,
+} from "../../services/localStorageService";
+
+export interface ConfigState {
+  darkMode: boolean;
+}
+
+const initialState: ConfigState = {
+  darkMode: getDarkModeFromLocalStorage(),
+};
+
+export const ConfigSlice = createSlice({
+  name: "config",
+  initialState,
+  reducers: {
+    setDarkMode: (state, action) => {
+      state.darkMode = true;
+      setDarkModeFromLocalStorage("true");
+    },
+    setLightMode: (state) => {
+      removeDarkModeFromLocalStorage();
+      state.darkMode = false;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { setDarkMode,setLightMode } = ConfigSlice.actions;
+
+export default ConfigSlice;
